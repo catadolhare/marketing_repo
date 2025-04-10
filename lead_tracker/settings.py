4cf66cabcaa121230b36ad731fc98110f8d0d49c
Catalina Dolhare <80982179+catadolhare@users.noreply.github.com>
2025-04-10 16:46:08 +0200

    Update settings.py

--- lead_tracker/settings.py
+++ lead_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8_vdmvq@jia!4*%#9*%^!=7fdkstj-nhi#wp_h373-fq8$&x@p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vicvolman.pythonanywhere.com']
+ALLOWED_HOSTS = ['catalinadolhare.pythonanywhere.com']
 
 
 # Application definition
